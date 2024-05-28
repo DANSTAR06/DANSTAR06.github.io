f65c5a9825129c04ad80f7ec8b45eb0bc4cc3062
DANSTAR06 <danwanuyu06@gmail.com>
2024-05-28 14:31:55 +0300

    add authorization.php

---
+++ authorization.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+function getAccessToken($consumerKey, $consumerSecret) {
+    $client = new Client();
+    $response = $client->request('GET', 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+     [
+        'auth' => [$consumerKey, $consumerSecret]
+    ]);
+
+    $body = json_decode($response->getBody());
+    return $body->access_token;
+}
+
+$consumerKey = 'y2aCl7n7auaSgrtc2hgBprZ0ofGm9X13tkUiTFJ2dcQUzaCs';
+$consumerSecret = 'Lt41jVDSbvRw97VaakGSiviAGrfmDYh5XwOkCFD4Mnjh5TRBqe6RGkLdvfaxiSPe';
+
+$accessToken = getAccessToken($consumerKey, $consumerSecret);
